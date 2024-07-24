14906a2f6cce16e1a65ed1d03b083e9301670bfa
Mateusz Mazurkiewicz <mateusz.mazurkiewicz@ext.esa.int>
2024-07-24 21:36:44 +0200

    Make items possible to throw away

--- main.py
+++ main.py
@@ -94,6 +94,13 @@ def cs_body():
                                             item_object.use(st.session_state['player_character'], st.session_state['world'])
                                             st.session_state['player_character'].inventory.remove(item)
                                             st.rerun()
+                                    if no > 1:
+                                        delete_button = "Throw away 1."
+                                    else:
+                                        delete_button = "Throw away."
+                                    if st.button(delete_button, key=f"inv_item_delete_{i}"):
+                                        st.session_state['player_character'].inventory.remove(item)
+                                        st.rerun()
             with col12:
                 st.markdown(st.session_state['player_character'].display2())
             with col13:
